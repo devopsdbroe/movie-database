a60ba0119ba6fbe412e8d9b3718a50f62f9e8683
Daniel Broe <dbroe1995@gmail.com>
2023-09-29 22:50:09 -0400

    Added release date and rating to MovieList

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,11 +1,22 @@
 import React from "react";
 
 const MovieList = ({ movies, onSelect }) => {
+	const formatDate = (dateString) => {
+		const options = { year: "numeric", month: "2-digit", day: "2-digit" };
+		return new Date(dateString).toLocaleDateString(undefined, options);
+	};
+
+	const formatRating = (rating) => {
+		return Math.round(rating * 10) / 10;
+	};
+
 	return (
 		<div>
 			{movies.map((movie) => (
 				<div key={movie.id} onClick={() => onSelect(movie)}>
-					{movie.title}
+					<h3>{movie.title}</h3>
+					<p>Release Date: {formatDate(movie.release_date)}</p>
+					<p>Average Rating: {formatRating(movie.vote_average)}</p>
 				</div>
 			))}
 		</div>
